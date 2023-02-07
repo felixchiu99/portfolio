69f74c674d3472a513bb12a85647e2c8ce354f42
felixchiu99 <felixchiu99@gmail.com>
2023-02-07 21:17:42 +0000

    app

--- src/App.js
+++ src/App.js
@@ -17,20 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Test</p>
       </header>
     </div>
   );
